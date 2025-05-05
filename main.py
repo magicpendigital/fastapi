7580a45f13ba38247117d594a8d20ace58bc87fd
magicpendigital <magicpendigital@gmail.com>
2025-05-05 11:50:23 +0700

    Mô tả thay đổi của bạn

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from flatlib.chart import Chart
+from flatlib import const
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+class Input(BaseModel):
+    date: str
+    time: str
+    lat: str
+    lon: str
+
+@app.post("/birth-chart")
+def birth_chart(data: Input):
+    chart = Chart(f'{data.date} {data.time}', data.lat, data.lon, hsys=const.HOUSES_PLACIDUS)
+    planets = {}
+    for obj in const.LIST_OBJECTS:
+        p = chart.get(obj)
+        planets[obj] = {
+            'sign': p.sign,
+            'lon': round(p.lon, 2),
+            'house': p.house
+        }
+    return {"chart": planets}
